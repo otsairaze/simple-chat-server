c8db2e30413d04d77d22e436e2b8cfa237612369
otsairaze <ssavelevdenchikk@gmail.com>
2025-02-16 20:59:21 +0300

    initial project

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: false });
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple-Chat')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(process.env.PORT ?? 7000);
+}
+bootstrap();
